c278429a80986fd107fcd05ffa2245d80f6ac062
Kevin Lin <Kevin.Lin@dickerdata.com.au>
2019-01-30 20:10:20 +1100

    comment out component import for two modules

--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MyCartComponent } from './my-cart/my-cart.component';
+// import { MyCartComponent } from './my-cart/my-cart.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MyCartComponent]
+  // declarations: [MyCartComponent]
 })
 export class ShoppingCartModule { }
